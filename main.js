9e3dac78d8785e56077d35b043dc57920e239db1
FJavohr <115878920+FJavohr@users.noreply.github.com>
2023-06-29 02:27:05 +0600

    Almost finish
    Interesting. A lot.

---
+++ main.js
@@ -0,0 +1,116 @@
+const cellElem = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const popup  = document.querySelector('.winning-popup')
+const restart = document.querySelector('button') 
+const x = 'x';
+const y = 'y';
+let XsWins = 0;
+let OsWins = 0;
+
+let turner;
+const combinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6]
+]
+// console.log(cell)
+
+const winnigMessageTextElement = document.querySelector('[data-winning-message-text]')
+
+startGame()
+
+function startGame() {
+  turner = false;
+  popup.classList.remove('show')
+  cellElem.forEach(cell => {
+    cell.classList.remove('x')
+    cell.classList.remove('y')
+    cell.removeEventListener('click', handleClick, {once: true})
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass();
+  winnigMessageTextElement.classList.remove('show')
+
+}
+
+function handleClick(event) {
+  const cell = event.target
+  const currentClass = turner ? y : x
+  placeMark(cell, currentClass)
+
+  if(checkWin(currentClass)){
+    endGame(false)
+  }
+  else if(isDraw()) {
+    endGame(true)
+  }
+  else {
+    switchMark()
+    setBoardHoverClass()  
+  }
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+function switchMark() {
+  turner = !turner
+}
+function setBoardHoverClass() {
+  board.classList.remove(x)
+  board.classList.remove(y)
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(x)
+  board.classList.remove(y)
+  if (turner) {
+    board.classList.add(y)
+  } else {
+    board.classList.add(x)
+  }
+}
+
+
+function checkWin(currentClass) {
+ return combinations.some(combination => {
+  return combination.every(index => {
+    return cellElem[index].classList.contains(currentClass);
+  })
+ })
+}
+
+function isDraw() {
+  return [...cellElem].every(cell => {
+    return cell.classList.contains(x) || cell.classList.contains(y)  
+  })
+}
+
+function endGame(draw) {
+  if(draw) {
+    winnigMessageTextElement.innerText = '! Draw !'
+  }else {
+    winnigMessageTextElement.innerText = `${turner ? "O's Wins" : "X's Wins!" }`
+  }
+  popup.classList.add('show')
+}
+
+restart.addEventListener('click', startGame)
+
+
+
+// restart.addEventListener('click', () => {
+//   board.classList.remove('x')
+//   board.classList.remove('y')
+  
+//   for(let hurma of cellElem ){
+//     return hurma.classList.remove('x') 
+//   }
+  
+//   startGame()
+// }) 
